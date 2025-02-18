ad486f34a4134f37518d9f5af82d9bbc2dc8ddab
margaritavnukova <margaritavnukova82@gmail.com>
2025-02-18 11:32:47 +0300

    created files and templates + README

---
+++ office_library_backend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="office_library_backend.MvcApplication" Language="C#" %>
---
+++ office_library_backend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(office_library_backend.Startup))]
+namespace office_library_backend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
